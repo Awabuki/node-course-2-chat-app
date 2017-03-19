c24e4741d9931f5c129fed23ce6df830fc97875a
Jeremy <suede@shaw.ca>
2017-03-19 12:19:00 -0500

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
-// Built in node module to work with paths, instead of using "../" to back up
-// module called PATH that comes with node
-const path = require('path');
+const path = require('path');  // Built in node module to work with paths, instead of using "../" to back up, module called PATH that comes with node
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT||3000; 
-
-//console.log(__dirname + '/../public');  // goes in and out
-//console.log(publicPath);  // goes directly to public
-
 var app = express();
-
+var server = http.createServer(app);  // server and app are almost identical, same calls and all.
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// register event listener. Get individual socket
+io.on('connection', (socket) => {
+	console.log('New user connected');
+	
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+		
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
 
-//module.exports = {app};
+
